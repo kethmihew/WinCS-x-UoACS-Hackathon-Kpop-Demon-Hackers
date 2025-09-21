925b64a61252a75b0732c8b959bc2c39507f678f
rhbi-auk <hamcheesesnadwich@outlook.co.nz>
2025-09-21 13:14:02 +1200

    Update Homepage.py

--- Main/Homepage.py
+++ Main/Homepage.py
@@ -63,8 +63,8 @@ class HomePage:
             friends_page_button = tk.Button(self.menu_buttons_frame_right, text="Friends Page")
             friends_page_button.grid(row=0, column=0, sticky="ew", pady=button_y_padding)
 
-            walk_timer_button = tk.Button(self.menu_buttons_frame_right, text="Walk Timer")
-            walk_timer_button.grid(row=1, column=0, sticky="ew", pady=button_y_padding)
+            shower_button = tk.Button(self.menu_buttons_frame_right, text="Shower")
+            shower_button.grid(row=1, column=0, sticky="ew", pady=button_y_padding)
 
             work_timer_button = tk.Button(self.menu_buttons_frame_right, text="Work Timer")
             work_timer_button.grid(row=2, column=0, sticky="ew", pady=button_y_padding)
